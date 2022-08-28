cf352d4b7270926db41f414fb613482eef939d27
lorenz <39259887+LoW-1@users.noreply.github.com>
2022-08-28 12:49:28 +0200

    etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxsModule } from '@ngxs/store';
+//import { ZooState } from './examples/zoo.state';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/shared/app.state.ts
@@ -0,0 +1,23 @@
+import { Action, State } from "@ngxs/store";
+
+export interface AppStateModel {
+    username: string;
+    orderId: number;
+    status?: 'pending' | 'confirmed' | 'declined'
+}
+
+@State<AppStateModel>({
+    name: 'app',
+    defaults: {
+        username: '',
+        orderId: Math.floor(Math.random() * 23000)
+    }
+
+})
+
+export class AppState {
+
+    //@Action(SetUsername)
+    //setUsername
+
+}
